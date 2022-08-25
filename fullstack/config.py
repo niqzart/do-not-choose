cfd13099b3d76a78eae972dd4097475efde31e2e
qzart45hi <qwert45hi@yandex.ru>
2022-08-25 18:50:20 +0300

    feat(x): more error handling & codes

--- fullstack/config.py
+++ fullstack/config.py
@@ -1,4 +1,8 @@
 from sys import modules
+from traceback import format_tb
+
+from flask import request
+from werkzeug.exceptions import NotFound
 
 from __lib__.flask_fullstack import Flask
 from __lib__.flask_fullstack.sqlalchemy import create_base, Sessionmaker, Session
@@ -11,6 +15,20 @@ app: Flask = Flask(__name__)
 app.config["TESTING"] = "pytest" in modules.keys()
 app.secrets_from_env("hope it's local")
 # app.configure_cors()
-
-app.configure_error_handlers(print)
 app.config["RESTX_INCLUDE_ALL_MODELS"] = True
+
+
+@app.errorhandler(NotFound)
+def on_not_found(_):
+    return app.return_error(404, "Not Found")
+
+
+@app.errorhandler(Exception)
+def on_any_exception(error: Exception):
+    error_text: str = (
+            f"Requested URL: {request.path}\n"
+            + "".join(format_tb(error.__traceback__))
+            + f"Error: {repr(error)}"
+    )
+    print(error_text)
+    return app.return_error(500, error_text if app.debug else "Error occurred, check the logs")
--- fullstack/simple.py
+++ fullstack/simple.py
@@ -1,4 +1,4 @@
-from flask_restx import Resource
+from flask_restx import Resource, inputs
 from flask_restx.reqparse import RequestParser
 
 from __lib__.flask_fullstack import ResourceController, PydanticModel
@@ -18,12 +18,13 @@ class SimpleResource(Resource):
         return SimpleModel(message="Hello World")
 
     parser = RequestParser()
-    parser.add_argument("error", type=bool, default=False, help="If true will cause a 500 error")
+    parser.add_argument("error", type=inputs.boolean, default=False,
+                        help="If true will cause a 500 error")
 
     @controller.doc_abort(500, "You asked to raise an error")
     @controller.argument_parser(parser)
     @controller.marshal_with(SimpleModel)
     def post(self, *, error: bool):
         if error:
-            raise ValueError()
-        return SimpleModel(message="I'm fine")
+            raise ValueError("User requested an error")
+        return SimpleModel(message="I'm fine"), 201
